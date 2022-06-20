dfebe87761dc766c0008216f3c73ae4d3fd471f3
agnesezimele <agnese.zimele@gmail.com>
2022-06-20 20:56:34 +0300

    commit

--- script.js
+++ script.js
@@ -11,12 +11,23 @@ let inputEmail = document.getElementById("email-input");
 let send = document.getElementById("guideSent");
 
 
-send.addEventListener("click", Download);
+send.addEventListener("click", Open);
 
-function Download(){
-  if (inputEmail.value == "") {
-    alert("empty");
+function Open (){
+  if (ValidateEmail(inputEmail)=== false) {
+    e.preventDefault();
   } else {
+    open('https://www.dropbox.com/sh/nrj1qda2ezpqvw2/AADX85lni5frZ2UMbo5D6AXta?dl=0');
 
+  }
+}
+
+function ValidateEmail(inputEmail) {
+let validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  if (inputEmail.value.match(validRegex)) {
+    return true;
+ } else {
+  alert("Invalid email address");
+    return false;
   }
 }
